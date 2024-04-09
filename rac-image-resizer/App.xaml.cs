9e390a821136731b22659aeeabe20ede1eec4b0c
dano <ganzcooler@protonmail.com>
2024-04-09 08:08:46 +0200

    Projektdateien hinzufügen.

---
+++ rac-image-resizer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace rac_image_resizer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ rac-image-resizer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace rac_image_resizer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
